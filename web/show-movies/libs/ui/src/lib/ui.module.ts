b84ab8fb01974e5a142068800b113bcae91d2147
jasper <jjasper22@gmail.com>
2019-09-19 23:30:51 +0300

    Added Clarity (https://v2.clarity.design/get-started) package

--- web/show-movies/libs/ui/src/lib/ui.module.ts
+++ web/show-movies/libs/ui/src/lib/ui.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule,  ClarityModule, BrowserAnimationsModule]
 })
 export class UiModule {}
