337b9d9926429ce46e76de8acde86bbf4bfca892
jasper <jjasper22@gmail.com>
2019-09-19 23:10:05 +0300

    Added Narwall workspace + ShowMovies app

---
+++ web/show-movies/apps/show-movies/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'show-movies-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'show-movies';
+}
