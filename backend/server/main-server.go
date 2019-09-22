cd203372e1cb2f30069dc32c99b34b3202e4e68b
jasper <jjasper22@gmail.com>
2019-09-22 22:11:32 +0300

    Fixed server.go and added VS Code launch configuration

--- backend/server/main-server.go
+++ backend/server/main-server.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
 
-	router.Static("/", "../../web/show-movies/dist/apps/show-movies/index.html")
+	router.Use(static.Serve("/", static.LocalFile("../../web/show-movies/dist/apps/show-movies/", true)))
+
+	router.NoRoute(func(c *gin.Context) {
+		c.Redirect(301, "/")
+	})
 
 	router.Run(":8080")
 }
