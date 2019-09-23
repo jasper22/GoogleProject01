fe8892bae83a649b85c2a59f53518f33fa662f43
Aleksandr <jjasper22@gmail.com>
2019-09-23 18:31:46 +0300

    Added some angular code

--- web/show-movies/apps/show-movies/src/app/app.module.ts
+++ web/show-movies/apps/show-movies/src/app/app.module.ts
@@ -1,13 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ClarityModule, ClrIconModule } from '@clr/angular';
 import { UiModule } from '@show-movies/ui';
 
+const appRoutes: Routes = [
+  {
+    path: 'home',
+    component: AppComponent,
+    data: { title: 'Heroes List' }
+  },
+  { path: '',
+    redirectTo: '/heroes',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, UiModule, ClarityModule, ClrIconModule],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    UiModule,
+    ClarityModule,
+    ClrIconModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
